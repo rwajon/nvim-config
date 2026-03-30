f48828dbb2a2829797902fb9fb8a6549fb8ddf5f
Jonathan Rwabahizi <jonathanrwabahizi@gmail.com>
2026-03-30 12:35:01 +0200

    update options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,38 +1,38 @@
-local opt = vim.opt
+local opt          = vim.opt
 
 -- Appearance
-opt.number         = true          -- show line numbers
-opt.relativenumber = false         -- absolute numbers
-opt.signcolumn     = "yes"         -- always show gutter
-opt.cursorline     = true          -- highlight current line
-opt.colorcolumn    = ""            -- no ruler by default
-opt.wrap           = false         -- no line wrap
-opt.scrolloff      = 8             -- keep lines visible above/below cursor
+opt.number         = true  -- show line numbers
+opt.relativenumber = false -- absolute numbers
+opt.signcolumn     = "yes" -- always show gutter
+opt.cursorline     = true  -- highlight current line
+opt.colorcolumn    = ""    -- no ruler by default
+opt.wrap           = false -- no line wrap
+opt.scrolloff      = 8     -- keep lines visible above/below cursor
 opt.sidescrolloff  = 8
 opt.termguicolors  = true
 
 -- Indentation
-opt.expandtab      = true          -- spaces not tabs
-opt.tabstop        = 4
-opt.shiftwidth     = 4
-opt.softtabstop    = 4
+opt.expandtab      = true -- spaces not tabs
+opt.tabstop        = 2
+opt.shiftwidth     = 2
+opt.softtabstop    = 2
 opt.smartindent    = true
 opt.autoindent     = true
 
 -- Search (case-insensitive unless uppercase present)
 opt.ignorecase     = true
 opt.smartcase      = true
-opt.hlsearch       = true          -- highlight matches
-opt.incsearch      = true          -- incremental search
+opt.hlsearch       = true -- highlight matches
+opt.incsearch      = true -- incremental search
 
 -- Files
 opt.encoding       = "utf-8"
 opt.fileencoding   = "utf-8"
-opt.undofile       = true          -- persistent undo
+opt.undofile       = true -- persistent undo
 opt.swapfile       = false
 opt.backup         = false
-opt.autoread       = true          -- reload files changed outside nvim
-opt.hidden         = true          -- allow switching buffers without saving
+opt.autoread       = true -- reload files changed outside nvim
+opt.hidden         = true -- allow switching buffers without saving
 
 -- Editor behaviour
 opt.mouse          = "a"           -- enable mouse
@@ -49,14 +49,14 @@ opt.cmdheight      = 1
 
 -- Folding (indent-based, all open by default)
 opt.foldmethod     = "indent"
-opt.foldlevel      = 99            -- start with all folds open
+opt.foldlevel      = 99 -- start with all folds open
 opt.foldenable     = true
 
 -- Whitespace rendering
 opt.list           = false
 
 -- Ruler / minimap approximation: just line info in statusline
-opt.ruler          = false         -- handled by statusline plugin
+opt.ruler          = false -- handled by statusline plugin
 
 -- Word wrap indicator
 opt.showbreak      = "↪ "
